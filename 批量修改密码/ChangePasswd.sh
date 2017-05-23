9882fd4a95f1e05653673d4b198c193917a74a94
youlinux <77719125@qq.com>
2017-05-23 09:42:44 +0800

    批量修改脚本
    通过命令，可以在一台跳板机上批量修改密码，并将所有的密码记录在跳板机上的一个文件上，方便整理。

---
+++ 批量修改密码/ChangePasswd.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+port=22
+
+# 在ip_list.txt文件中，可以将我们所有服务器的IP地址写出来 
+for IP in `cat ip_list.txt`;do
+
+# 使用命令生成一个随机密码并赋值给TMP_PWD
+TMP_PWD=`mkpasswd -l 18 -C 8`
+
+# R_PWD.txt 文件的格式 : 用户名:密码
+echo "root:${TMP_PWD}" > R_PWD.txt
+
+	if [ $? = 0 ]; then
+		# 前提是执行此脚本的服务器要和其他所有的服务器互信并且可以免密码登陆
+		# 使用ssh密码在每台需要修改密码的服务器上执行一个命令
+		# chpasswd命令 读入 R_PWD.txt 文件(格式为用户名:密码)
+		ssh -p${port} ${IP} chpasswd < R_PWD.txt
+		
+		# 将修改密码的时间,服务器IP,及新的密码记录到R_Server.log 文件中
+		echo -e "$(date "+%Y-%m-%d %H:%M:%S")\t${IP}\t${TMP_PWD}\t" >> R_Server.log
+		
+	else
+	
+		echo -e "$(date "+%Y-%m-%d %H:%M:%S")\t${IP} R_PWD.txt is create fail\tplease check!\t" >> M_pass.log
+	
+	fi
+
+	if [ $? = 0 ]; then	
+		# 查看密码修改成功的服务器
+		echo -e "$(date "+%Y-%m-%d %H:%M:%S")\tThe ${IP} passwd is modify OK\t" >> M_pass.log
+	else
+		# 查看密码修改失败的服务器
+		echo -e "$(date "+%Y-%m-%d %H:%M:%S")\tThe ${IP} passwd is modify fail\tplease check!\t" >> M_pass.log
+	fi 
+
+done
